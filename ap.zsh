0e22e1bd0df43fdc52b08f40f9b26278295e1e3f
flw <flw@cpan.org>
2022-02-07 17:53:42 +0800

    feat: add more default commands

--- ap.zsh
+++ ap.zsh
@@ -4,25 +4,38 @@ auto-pager-wrapper() {
     if (( ! $+commands[$name] )) ; then
         return
     fi
-    if [[ "x$grc" == "xgrc" ]]; then
+    if [[ "x$grc" == "xgrc" && (( $+commands[grc] )) ]]; then
         ap grc $name $@
     else
         ap $name $@
     fi
 }
 
-AUTO_PAGER_CMDS_DEFAULT=(
-    go make
-    ps lsof netstat
+AUTO_PAGER_CMDS_DEFAULT_GRC=(
+    df du env id last lsof mount ps sysctl
+    diff tar
+    cc gcc g++ make go
+    curl dig ifconfig iostat ip iptables iptables-save netstat
+    ping ping6 tcpdump traceroute traceroute6 whois
+    docker docker-compose docker-machine kubectl
 )
 
-if [[ ${#AUTO_PAGER_CMDS[@]} -eq 0 ]]; then
-    AUTO_PAGER_CMDS=($AUTO_PAGER_CMDS_DEFAULT)
-fi
+AUTO_PAGER_CMDS_DEFAULT=(
+    cargo rustc
+)
 
-for cmd in $AUTO_PAGER_CMDS; do
-    $cmd() { auto-pager-wrapper - $@ }
-done
+if [[ ${#AUTO_PAGER_CMDS[@]} -gt 0 ]]; then
+    for cmd in $AUTO_PAGER_CMDS; do
+        $cmd() { auto-pager-wrapper - $@ }
+    done
+else
+    for cmd in $AUTO_PAGER_CMDS_DEFAULT; do
+        $cmd() { auto-pager-wrapper - $@ }
+    done
+    for cmd in $AUTO_PAGER_CMDS_DEFAULT_GRC; do
+        $cmd() { auto-pager-wrapper grc $@ }
+    done
+fi
 
 for cmd in $AUTO_PAGER_CMDS_EXTRA; do
     $cmd() { auto-pager-wrapper - $@ }
