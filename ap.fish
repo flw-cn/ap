05bd0627ed93ed56c17505657c815472dabcf6c2
flw <flw@cpan.org>
2022-02-10 17:32:57 +0800

    feat: support more commands

--- ap.fish
+++ ap.fish
@@ -1,13 +1,15 @@
-set -q AUTO_PAGER_CMDS; or set AUTO_PAGER_CMDS \
-    ls tree \
-    go cargo rustc
+set -q AUTO_PAGER_CMDS; or set AUTO_PAGER_CMDS  \
+    ls tree find fd cat                         \
+    go cargo rustc                              \
+    grep egrep fgrep rg ack ag ucg pt sift      \
+    brew port emerge apt apt-get aptitude
 
-set -q AUTO_PAGER_CMDS_WITH_GRC; or set AUTO_PAGER_CMDS_WITH_GRC \
-    df du env id last lsof mount ps sysctl \
-    diff tar \
-    cc gcc g++ make \
-    curl dig ifconfig iostat ip iptables iptables-save netstat \
-    ping ping6 tcpdump traceroute traceroute6 whois \
+set -q AUTO_PAGER_CMDS_WITH_GRC; or set AUTO_PAGER_CMDS_WITH_GRC    \
+    df du env id last lsof mount ps sysctl                          \
+    diff tar                                                        \
+    cc gcc g++ make mvn                                             \
+    curl dig ifconfig iostat ip iptables iptables-save netstat      \
+    ping ping6 tcpdump traceroute traceroute6 whois                 \
     docker docker-compose docker-machine kubectl
 
 for _cmd in $AUTO_PAGER_CMDS $AUTO_PAGER_CMDS_EXTRA;
--- ap.zsh
+++ ap.zsh
@@ -1,12 +1,14 @@
 declare -p AUTO_PAGER_CMDS >/dev/null 2>&1 || AUTO_PAGER_CMDS=(
-    ls tree
+    ls tree find fd cat
     go cargo rustc
+    grep egrep fgrep rg ack ag ucg pt sift
+    brew port emerge apt apt-get aptitude
 )
 
 declare -p AUTO_PAGER_CMDS_WITH_GRC >/dev/null 2>&1 || AUTO_PAGER_CMDS_WITH_GRC=(
     df du env id last lsof mount ps sysctl
     diff tar
-    cc gcc g++ make
+    cc gcc g++ make mvn
     curl dig ifconfig iostat ip iptables iptables-save netstat
     ping ping6 tcpdump traceroute traceroute6 whois
     docker docker-compose docker-machine kubectl
